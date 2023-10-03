bb38d42269427b262c8f88abf1541fd9c10e616c
Mayanktyagi1998 <mktyagi112@gmail.com>
2023-10-03 22:21:51 +0530

    commitchanges

---
+++ IntellipatProject1/src/test/java/AutomationBasics/ForEachLoop.java
@@ -0,0 +1,14 @@
+package AutomationBasics;
+
+public class ForEachLoop {
+
+    public static void main(String[] args) {
+
+
+        int[] a = {34, 22, 56};
+
+        for (int k : a) {
+            System.out.println(k);
+        }
+    }
+}
