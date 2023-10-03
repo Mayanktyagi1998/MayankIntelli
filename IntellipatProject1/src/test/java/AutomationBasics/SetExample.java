3a1d0789c9b829a2738ab7d1580e61355f87d997
Mayanktyagi1998 <mktyagi112@gmail.com>
2023-10-03 19:09:58 +0530

    CommitChangesInSetExample

--- IntellipatProject1/src/test/java/AutomationBasics/SetExample.java
+++ IntellipatProject1/src/test/java/AutomationBasics/SetExample.java
@@ -1,5 +1,16 @@
 package AutomationBasics;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class SetExample {
-    punbliv
+public static void main(String args[])
+{
+    Set<Integer> in = new HashSet<Integer>();
+    in.add(32);
+    in.add(54);
+    System.out.println(in);
+
+}
+
 }
